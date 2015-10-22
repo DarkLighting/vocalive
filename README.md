07979a9883af3ef817d6f049c453a677f93836fd
acaetano <ailtoncaetanos@gmail.com>
2015-10-22 17:32:50 -0200

    First functional version

---
+++ README.md
@@ -0,0 +1,15 @@
+```
+usage: vocalive.py [-h] [-m Length] [-o File] Address
+
+Live Vocabulary Observer
+
+positional arguments:
+  Address               Web address to learn from
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -m Length             Word minimum length. Default = 2
+  -o File, --output File
+                        File to save learned words
+
+```
--- vocalive.py
+++ vocalive.py
@@ -5,18 +5,19 @@ import requests;
 from bs4 import BeautifulSoup;
 import os;
 import sys;
+from unidecode import unidecode;
 # -*- coding: utf-8 -*-
 
 class Manager:
-    """ Manages bots """
+    """ Manages bots and labs """
     args = '';
     bots = list();
 
     def __init__(self):
         """ Where everything begins... """
         parser = argparse.ArgumentParser( description='Live Vocabulary Observer' );
-        parser.add_argument( '-m', metavar='Length',  help='Word minimum length' );
-        parser.add_argument( '-o', '--output', metavar='File', default=2, help='File to save learned words' );
+        parser.add_argument( '-m', metavar='Length', default=2, help='Word minimum length. Default = 2' );
+        parser.add_argument( '-o', '--output', metavar='File', help='File to save learned words' );
         parser.add_argument( 'url', metavar='Address', help='Web address to learn from' );
         self.args = parser.parse_args();
 
@@ -27,7 +28,6 @@ class Manager:
         self.bots.append( Bot( self, url ) );
 
     def write_words( self, text, encoding ):
-        print encoding;
         try:
             with open( self.args.output, 'w+' ) as content:
                     for word in text:
@@ -57,12 +57,16 @@ class Bot:
 
     def fetch(self):
         self.check_url();
+        answer = '';
         try:
             self.headers = {'Accept-Charset': 'utf-8'}
             self.answer = requests.get( self.address, params=self.headers )
             if (self.answer.status_code != 200):
                 raise IOError
             self.souped_page = BeautifulSoup(self.answer.text, 'html.parser');
+            for script in self.souped_page(["script", "style"]):
+                script.extract()
+            self.souped_page = BeautifulSoup(str(self.souped_page), 'html.parser');
             self.text = self.souped_page.get_text();
             self.send_to_lab()
             exit();
@@ -76,29 +80,140 @@ class Bot:
 class Laboratory:
     """ Text processing """
 
-    manager = '';
-    text = '';
-    encoding = '';
+    manager = str();
+    text = str();
+    encoding = str();
     wordlist = set();
     count = int();
 
     def __init__( self, manager, text, encoding ):
         self.manager = manager;
         self.encoding = encoding;
-        self.analyse( text );
+        self.analyse( text.lower() );
 
 
     def analyse( self, text ):
         separated_words = self.breakdown( text );
-        unique_words = self.discard_repetition( separated_words );
+        clean_words = self.strip_punctuation( separated_words );
+        unaccented = self.strip_accentuation( clean_words );
+        unique_words = self.discard_repetition( unaccented );
         eligible_words = self.minimum_length( unique_words, self.manager.args.m );
-        self.wordlist = self.sort_data( eligible_words ); 
+        sorted_strings = self.sort_data( eligible_words );
+        self.wordlist = self.digits_out( sorted_strings ); 
         self.count = self.count_words( self.wordlist );
         self.manager.get_results( self.wordlist, self.encoding );
         
 
     def breakdown( self, text ):
+        text = self.convert_to_space( text );
         return text.split();
+
+    def strip_punctuation( self, text ):
+        treated = list();
+        for word in text:
+            treated.append( self.remove_chars( word ) );
+        return treated;
+
+    def convert_to_space( self, string ):
+        space_chars = { ord('.'): ord(' '), 
+                        ord('_'): ord(' '),
+                        ord('/'): ord(' '),
+                        ord('-'): ord(' ')};
+        return string.translate( space_chars );
+
+    def remove_chars(self, string ):
+        blocked_chars = { ord('!'): None,
+                          ord('\"'): None,
+                          ord('#'): None,
+                          ord('$'): None,
+                          ord('%'): None,
+                          ord('&'): None,
+                          ord('\''): None,
+                          ord('('): None,
+                          ord(')'): None,
+                          ord('*'): None,
+                          ord('+'): None,
+                          ord(','): None,
+                          ord(':'): None,
+                          ord(';'): None,
+                          ord('<'): None,
+                          ord('='): None,
+                          ord('>'): None,
+                          ord('?'): None,
+                          ord('@'): None,
+                          ord('['): None,
+                          ord('\\'): None,
+                          ord(']'): None,
+                          ord('^'): None,
+                          ord('`'): None,
+                          ord('{'): None,
+                          ord('|'): None,
+                          ord('}'): None,
+                          ord('~'): None,
+                          ord('\xa9'): None,
+                          ord('\xaa'): None,
+                          ord('\xab'): None,
+                          ord('\xae'): None,
+                          ord('\xb0'): None,
+                          ord('\xb2'): None,
+                          ord('\xb3'): None,
+                          ord('\xba'): None,
+                          ord('\xbb'): None,
+                          ord('\xb9'): None};
+        return string.translate( blocked_chars );
+
+    def strip_accentuation( self, string ):
+        treated = list();
+        for word in string:
+            treated.append( self.translate_accents( word ) );
+        #return string.translate( accents );
+        return treated;
+        #return unidecode( string );
+
+    def translate_accents( self, string ):
+        accents = { 
+                    ord('\xe7'): u'c',
+                    ord('\xc0'): ord('A'),
+                    ord('\xc0'): ord('A'),
+                    ord('\xc1'): ord('A'),
+                    ord('\xc2'): ord('A'),
+                    ord('\xc3'): ord('A'),
+                    ord('\xc8'): ord('E'),
+                    ord('\xc9'): ord('E'),
+                    ord('\xca'): ord('E'),
+                    ord('\xcc'): ord('I'),
+                    ord('\xcd'): ord('I'),
+                    ord('\xce'): ord('I'),
+                    ord('\xd2'): ord('O'),
+                    ord('\xd3'): ord('O'),
+                    ord('\xd4'): ord('O'),
+                    ord('\xd5'): ord('O'),
+                    ord('\xd9'): ord('U'),
+                    ord('\xda'): ord('U'),
+                    ord('\xdb'): ord('U'),
+                    ord('\xdc'): ord('U'),
+                    ord('\xe0'): ord('a'),
+                    ord('\xe1'): ord('a'),
+                    ord('\xe2'): ord('a'),
+                    ord('\xe3'): ord('a'),
+                    ord('\xe8'): ord('e'),
+                    ord('\xe9'): ord('e'),
+                    ord('\xea'): ord('e'),
+                    ord('\xec'): ord('i'),
+                    ord('\xed'): ord('i'),
+                    ord('\xee'): ord('i'),
+                    ord('\xf1'): ord('n'),
+                    ord('\xf2'): ord('o'),
+                    ord('\xf3'): ord('o'),
+                    ord('\xf4'): ord('o'),
+                    ord('\xf5'): ord('o'),
+                    ord('\xf9'): ord('u'),
+                    ord('\xfa'): ord('u'),
+                    ord('\xfb'): ord('u'),
+                    ord('\xfc'): ord('u')};
+        return string.translate( accents );
+        #return treated;
+ 
 
     def discard_repetition( self, words ):
         temp = set();
@@ -107,10 +222,9 @@ class Laboratory:
                 temp.add( word );
         return temp;
 
-    def minimum_length( self, words, number ):
+    def minimum_length( self, words, number=2 ):
         minimum_length = int( number );
         temp = set();
-        print minimum_length;
         for word in words:
             if( len( word ) >= minimum_length ):
                 temp.add(word);
@@ -120,6 +234,13 @@ class Laboratory:
     def sort_data( self, unique ):
         return sorted( unique ); 
 
+    def digits_out( self, strings ):
+        treated = list();
+        for word in strings:
+            if( not word.isdigit() ):
+                treated.append( word );
+        return treated;
+
     def count_words( self, words ):
         number = int();
         number = len( words );
@@ -128,7 +249,7 @@ class Laboratory:
 
 
 def end( reason ):
-    print '\n[-] ' + reason;
+    print('\n[-] ' + reason);
     exit();
 
 if __name__ == "__main__":
